00b3738c51bdde3b774b0c738cd3fc62e54166ec
3sho7mi8 <hiro.pinot@gmail.com>
2017-07-18 12:31:24 +0900

    Add SendGrid to ActionMailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# SendGrid to ActionMailer
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'mukurosii.io',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
