4c36250114b8741fda0f0d8d178622c97d5760e9
3sho7mi8 <hiro.pinot@gmail.com>
2017-07-23 17:15:54 +0900

    Add Registration Function

--- app/controllers/mail_news_controller.rb
+++ app/controllers/mail_news_controller.rb
@@ -1,4 +1,25 @@
 class MailNewsController < ApplicationController
   def index
+    if params[:email]
+      email = params[:email]
+      mail = MailingList.new
+      unless mail.contact_present?(email)
+        mail.add_contact(email)
+        redirect_to mail_news_path, notice: 'Add Mailing List!!'
+      else
+        redirect_to mail_news_path, notice: 'Mail Address was register'
+      end
+    end
+
+    if params[:remove_email]
+      email = params[:remove_email]
+      mail = MailingList.new
+      unless mail.contact_present?(email)
+        redirect_to mail_news_path, notice: 'Mail Address is not register'
+      else
+        mail.remove_contact(email)
+        redirect_to mail_news_path, notice: 'Remove Your Mail to Mailing List!!'
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   root to: 'mail_news#index'
 
+  get 'mail_news' => 'mail_news#index'
+  post 'mail_news' => 'mail_news#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
